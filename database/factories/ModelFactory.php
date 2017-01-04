74eead8f4493629074196661a72d36bdb70f795c
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-01-04 10:58:15 +0700

    factory & rollback on User

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,8 +17,16 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 
     return [
         'name' => $faker->name,
+        'role' => 'sale',
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => $password ?: $password = bcrypt('admin123'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+    return [
+        'code' => $faker->name,
+        'name' => 'sale'
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,30 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder{
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run(){
+        $this->v1BasicNeed(true);
+    }
+
+    private function v1BasicNeed($rollback = false){
+        if(!$rollback){
+            factory(App\User::class, 10)->create();
+        }
+
+        if($rollback){
+            DB::statement("SET FOREIGN_KEY_CHECKS = 0;");
+            DB::statement("SET AUTOCOMMIT = 0;");
+            DB::transaction(function (){
+                $truncateTpl = "TRUNCATE TABLE %s;";
+                DB::statement(sprintf($truncateTpl, 'users'));
+                DB::statement("SET FOREIGN_KEY_CHECKS = 1;");
+            });
+            DB::statement("SET AUTOCOMMIT = 1;");
+        }
     }
 }
