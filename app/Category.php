2092be4f041aaee7c3df28d80a4665e104282cb2
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-01-04 11:37:20 +0700

    category default code, cannot set on code

--- app/Category.php
+++ app/Category.php
@@ -5,7 +5,14 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use App\ApiUtils\ModelCode;
 
-class Category extends Model
-{
+class Category extends Model{
     use ModelCode;
+
+    protected $table = 'categories';
+    protected $fillable = ['code', 'name'];
+
+    public function __construct(array $attributes = []){
+        parent::__construct($attributes);
+        $this->attributes['code'] = $this->getUniqueCode();
+    }
 }
